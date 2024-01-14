3bd9a8b5fab84a0cbca90a1fd6b1499b68a18b66
Ranmiz134 <155522069+Ranmiz134@users.noreply.github.com>
2024-01-14 12:51:45 +0200

    Add files via upload

---
+++ src/King.java
@@ -0,0 +1,27 @@
+public class King extends ConcretePiece {
+
+    /**
+     * Constructor for pawn.
+     *
+     * owner The player who owns this piece-Player 1.
+     * type the symbol of pawn.
+     *
+     */
+
+    public King(Player playerOne) {
+        this.owner = playerOne;
+        this.dis = 0;
+    }
+
+    /**
+     * @return the symbol of King piece.
+     */
+    public String getType() {
+        return "♔";  // Unicode character for King.
+    }
+
+    @Override
+    public String toString() {
+        return "K"  + sn + ": ";
+    }
+}
