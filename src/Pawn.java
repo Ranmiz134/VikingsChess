32526276cacdb4806b1ccd8c129ce2fd7a750658
Ranmiz134 <155522069+Ranmiz134@users.noreply.github.com>
2024-01-31 17:00:06 +0200

    31/1

---
+++ src/Pawn.java
@@ -0,0 +1,52 @@
+public class Pawn extends ConcretePiece {
+
+
+    /**
+     * Constructor for pawn.
+     *
+     * owner The player who owns this piece.
+     * type the symbol of pawn.
+     *
+     */
+    public Pawn(Player owner) {
+        this.owner = owner;
+        this.dis = 0;
+        this.kills = 0;
+    }
+
+    /**
+     * The function adds a kill to the pawn
+     */
+    public void addKill() {
+        kills++;
+    }
+
+    /**
+     * The function remove a kill to the pawn
+     */
+    public void removeKill() {
+        kills--;
+    }
+
+    /**
+     * return how many kills their is to the pawn
+     */
+    public int getKills() {
+        return kills;
+    }
+
+    /**
+     * @return the symbol of pawn piece.
+     */
+    public String getType() {
+        if(this.owner.isPlayerOne())
+            return "♙";
+
+        return "♟";
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
+}
